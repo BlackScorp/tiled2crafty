35b979aaa0f0af1eabc7afd72446bff9e535c1ad
BlackScorp <witalimik@web.de>
2012-06-11 12:47:15 +0200

    testing polygons

--- render/diamondiso.js
+++ render/diamondiso.js
@@ -9,20 +9,20 @@ Crafty.extend({
             w:0,
             h:0
         },
-        _vp:{
+        _o:{
             x:0,
             y:0
         },
-        _o:{
+        _pos:{
             x:0,
             y:0
         },
 
         init:function(tw, th,mw,mh){
-           this._t.w = tw;
-            this._t.h = th||tw/2;
-            this._m.w = mw;
-            this._m.h = mh || mh;
+            this._t.w = parseInt(tw);
+            this._t.h = parseInt(th)||tw/2;
+            this._m.w = parseInt(mw);
+            this._m.h = parseInt(mh) || parseInt(mw);
             
             this._o.x = mw*tw/2-Crafty.viewport.x;
             this._o.y = th-Crafty.viewport.y;
@@ -34,10 +34,13 @@ Crafty.extend({
              
             var pos = this.pos2px(x,y);
             var offset = obj.__offset;
-          
-            obj.attr({
+            this._pos = {
                 x:pos.left-obj.w/2-offset[0],
-                y:pos.top-obj.h-offset[1],
+                y:pos.top-obj.h-offset[1]
+            }
+            obj.attr({
+                x:this._pos.x,
+                y:this._pos.y,
                 z:y
             });
             
@@ -52,6 +55,17 @@ Crafty.extend({
                 top:~~(t+this._o.y),
                 left:~~(l+this._o.x)
             }
+        },
+        polygon:function(){
+        
+            var p =[
+                [this._pos.x - 0,this._pos.y -this._t.h/2],
+                [this._pos.x -this._t.w/2,this._pos.y -0],
+                [this._pos.x -this._t.w,this._pos.y -this._t.h/2],
+                [this._pos.x -this._t.w/2,this._pos.y -this._t.h]
+            ]
+         
+            return new Crafty.polygon(p);
         }
        
     }
--- render/game.js
+++ render/game.js
@@ -7,27 +7,40 @@ $(function(){
     var map = frontier_outpost.metadata;
  
     var iso = Crafty.diamondIso.init(map.tilewidth,map.tileheight,map.width,map.height);
-   //  Crafty.background("url(frontier_outpost.png) #000 top left no-repeat");
-  // var tile = Crafty.e("2D","DOM",144);
-   //iso.place(tile,0,0);
-     var i = 0;
+    //  Crafty.background("url(frontier_outpost.png) #000 top left no-repeat");
+    // var tile = Crafty.e("2D","DOM",241,"Collision","WiredHitBox");
+   // iso.place(tile,5,5);
+   // tile.collision(iso.polygon());
+    var i = 0;
     for(var y = 0;y<64;y++){
         for(var x = 0;x<64;x++){
             var object = objects[i];
             var collision = collisions[i];
             if(object > 0){
-              var tile = Crafty.e("2D","DOM",object);//.text("X:"+x+"/Y:"+y);
-              if(collision > 0){
-                  tile.addComponent("Collision","Solid");
-              }
-              iso.place(tile,x,y); 
+                var tile = Crafty.e("2D","DOM",object);//.text("X:"+x+"/Y:"+y);
+            
+                iso.place(tile,x,y); 
+                if(collision > 0){
+                  tile.addComponent("Collision","Solid","WiredHitBox").collision(iso.polygon());
+                }
             }
             i++;
         }
     }
     
- 
-  $('#cr-stage').css({'overflow':'scroll'});
-    $('#cr-stage > div').css({'background':'url(frontier_outpost.png) #000 top left no-repeat','width':'4096px','height':'2048px','top':'0px','left':'0px'});
-    $('#cr-stage > canvas').css({'zIndex':2});
+ /*
+    $('#cr-stage').css({
+        'overflow':'scroll'
+    });
+    $('#cr-stage > div').css({
+        'background':'url(frontier_outpost.png) #000 top left no-repeat',
+        'width':'4096px',
+        'height':'2048px',
+        'top':'0px',
+        'left':'0px'
+    });
+    $('#cr-stage > canvas').css({
+        'zIndex':2
+    });
+    */
 });
