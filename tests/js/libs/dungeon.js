0fb3f099e357e30a5f6842c1e8e971d7f1be8168
BlackScorp <witalimik@web.de>
2012-07-04 16:46:10 +0200

    fixed rooms

--- tests/js/libs/dungeon.js
+++ tests/js/libs/dungeon.js
@@ -2,10 +2,11 @@ Crafty.extend({
     dungeon:{
         map:[],
         collision_map:[],
-        w:50,
-        h:50,
+        rooms:{},
+        w:100,
+        h:100,
         tiles:{
-             unused:0,
+            unused:0,
             empty:1,
             solid:2,
             wall:{
@@ -39,19 +40,17 @@ Crafty.extend({
                     this.setCell(x,y,this.tiles.unused);
                 }
             }
-            if(this.createRoom(11,11,'n')) console.log(this.map);
+            this.createRoom(11,11);
+            this.createRoom(40,40);
+                      this.createRoom(10,30);
+            console.log(this.rooms);
             return this;
         },
-        createRoom:function(x,y,direction){
+        createRoom:function(x,y){
             var w = Crafty.math.randomInt(4, this.room.w);
             var h = Crafty.math.randomInt(4, this.room.h);
-            var d = 'n';
-          
-            var i = this.directions.indexOf(direction);
-            if( i !== -1){
-            
-                d = this.directions[i];
-            }
+            var d = Crafty.math.randomElementOfArray(this.directions);
+      
             var yStart = 0;
             var yEnd = 0;
             var xStart = 0;
@@ -61,7 +60,10 @@ Crafty.extend({
             var tx = 0;
             var tyl = 0;
             var txl = 0;
-           
+            var room = 'Y'+y+'X'+x;
+            var rx = 0;
+            var ry = 0;
+            var data = [];
             switch(d){
                 case 'n':{
                     yStart = ~~(y);
@@ -78,20 +80,127 @@ Crafty.extend({
                         }
                     }
                     
+        
                     for( ty = yStart,tyl = yEnd;ty>tyl;ty-- ){
+                       
                         for( tx = xStart,txl = xEnd;tx<txl;tx++){
                             if(tx == xStart) tile = this.tiles.wall.w;
                             else if(tx == xEnd) tile = this.tiles.wall.e;
                             else if(ty == yStart) tile = this.tiles.wall.n;
                             else if(ty == yEnd) tile = this.tiles.wall.s;
                             else tile = this.tiles.empty;
-                            
-                         
                             this.setCell(tx,ty,tile);
+                            
+                            data[(rx + w * ry)] = tile;
+                            rx++;
+                        }
+                        ry ++;
+                    }
+                    this.rooms[room] = {size:{w:w,h:h},data:data};
+                    break;
+                }
+                case 'e':{
+          
+                    yStart = ~~(y-h/2);
+                    yEnd = ~~(y+(h+1)/2);
+                    xStart = ~~(x);
+                    xEnd = ~~(x+w);
+                  
+                    for(ty = yStart,tyl = yEnd;ty<tyl;ty++ ){
+                        if(ty < 0 || ty > this.h) return false;
+                        for(tx = xStart,txl = xEnd;tx<txl;tx++){
+                            if(tx < 0 || tx > this.w) return false;
+                            if(!this.getCell(tx,ty,this.tiles.unused)) return false;
                         }
                     }
-
+                 
+    
+                    for( ty = yStart,tyl = yEnd;ty<tyl;ty++ ){
+                        for( tx = xStart,txl = xEnd;tx<txl;tx++){
+                            if(tx == xStart) tile = this.tiles.wall.w;
+                            else if(tx == xEnd) tile = this.tiles.wall.e;
+                            else if(ty == yStart) tile = this.tiles.wall.n;
+                            else if(ty == yEnd) tile = this.tiles.wall.s;
+                            else tile = this.tiles.empty;
+                            this.setCell(tx,ty,tile);
+                            
+                           data[(rx + w * ry)] = tile;
+                            rx++;
+                        }
+                        ry++;
+                    }
+                    this.rooms[room] = {size:{w:w,h:h},data:data};
+                    break;
+                }
+                 
+                case 's':{
+     
+                    yStart = ~~(y);
+                    yEnd = ~~(y+h);
+                    xStart = ~~(x-w/2);
+                    xEnd = ~~(x+(w+1)/2);
                   
+                    for(ty = yStart,tyl = yEnd;ty<tyl;ty++ ){
+                        if(ty < 0 || ty > this.h) return false;
+                        for(tx = xStart,txl = xEnd;tx<txl;tx++){
+                            if(tx < 0 || tx > this.w) return false;
+                           
+                            if(!this.getCell(tx,ty,this.tiles.unused)) return false;
+                        }
+                    }
+                
+     
+                    for( ty = yStart,tyl = yEnd;ty<tyl;ty++ ){
+                        for( tx = xStart,txl = xEnd;tx<txl;tx++){
+                            if(tx == xStart) tile = this.tiles.wall.w;
+                            else if(tx == xEnd) tile = this.tiles.wall.e;
+                            else if(ty == yStart) tile = this.tiles.wall.n;
+                            else if(ty == yEnd) tile = this.tiles.wall.s;
+                            else tile = this.tiles.empty;
+                            this.setCell(tx,ty,tile);
+                            
+                        data[(rx + w * ry)] = tile;
+                            rx++;
+                        }
+                        ry++;
+                    }
+                    this.rooms[room] = {size:{w:w,h:h},data:data};
+
+                    break;
+                }
+                case 'w':{
+                
+                    yStart = ~~(y-h/2);
+                    yEnd = ~~(y+(h+1)/2);
+                    xStart = ~~(x);
+                    xEnd = ~~(x-w);
+                  
+                    for(ty = yStart,tyl = yEnd;ty<tyl;ty++ ){
+                        if(ty < 0 || ty > this.h) return false;
+                        for(tx = xStart,txl = xEnd;tx>txl;tx--){
+                            if(tx < 0 || tx > this.w) return false;
+                           
+                            if(!this.getCell(tx,ty,this.tiles.unused)) return false;
+                        }
+                    }
+                   
+             
+                    for( ty = yStart,tyl = yEnd;ty<tyl;ty++ ){
+                       
+                        for( tx = xStart,txl = xEnd;tx>txl;tx--){
+                            if(tx == xStart) tile = this.tiles.wall.w;
+                            else if(tx == xEnd) tile = this.tiles.wall.e;
+                            else if(ty == yStart) tile = this.tiles.wall.n;
+                            else if(ty == yEnd) tile = this.tiles.wall.s;
+                            else tile = this.tiles.empty;
+                            this.setCell(tx,ty,tile);
+                       
+                       data[(rx + w * ry)] = tile;
+                            rx++;
+                        }
+                        ry++;
+                    }
+                    this.rooms[room] = {size:{w:w,h:h},data:data};
 
                     break;
                 }
