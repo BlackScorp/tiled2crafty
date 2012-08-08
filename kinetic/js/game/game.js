b7bd9bcaf7b96369759d2462f5af5da5c56e03b7
BlackScorp <witalimik@web.de>
2012-08-08 15:30:33 +0200

    added loader

--- kinetic/js/game/game.js
+++ kinetic/js/game/game.js
@@ -11,7 +11,7 @@ $(function(){
     var loader = new Kinetic.Loader(files);
 
     loader.onProgress(function(data){
-        console.log(data.name);
+        console.log(data);
     });
     loader.onError(function(data){
         console.warn("Error on Load "+data.name);
@@ -26,20 +26,53 @@ $(function(){
 
 var Game = function(){
     this.stage = new Kinetic.Stage({
-       container:'game',
-       width:800,
-       height:600
+        container:'game',
+        width:800,
+        height:600
     });
+    this.paused = false;
+    this.backgroundLayer = new Kinetic.Layer({
+        name:'background' 
+    });
+    this.objectLayer = new Kinetic.Layer({
+        name:'object' 
+    });
+    var sort = function(){
+        this.children.sort(function(a,b){
+            return a.attrs.zIndex - b.attrs.zIndex;
+        })
+    };
+    this.backgroundLayer.beforeDraw(sort);
+    this.objectLayer.beforeDraw(sort);
+    this.stage.add(this.backgroundLayer);
+    this.stage.add(this.objectLayer);
+    
+    var image = new Kinetic.Image({
+        width:64,
+        height:128,
+        image:Kinetic.Assets["img/grassland.png"],
+        crop:{
+            x:0,y:0,width:64,height:128
+        },
+        x:0,
+        y:0
+    });
+ 
+    this.backgroundLayer.add(image);
+    this.stage.draw();
 }
 Game.prototype.update = function(){
     
     }
 Game.prototype.start = function(){
-    
-    }
+    this.stage.onFrame(function(){
+        if(!this.paused) return;
+    });
+    this.stage.start();
+}
 Game.prototype.pause = function(){
-    
-    }
+    this.paused = true;
+}
 Game.prototype.unpause = function(){
-    
-    }
+    this.paused = false;
+}
