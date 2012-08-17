b29caf174870ddf0a9cb96f1a1df26dd9dcdd176
BlackScorp <witalimik@web.de>
2012-08-17 12:35:40 +0200

    added loading bar

---
+++ kinetic/js/game/files.js
@@ -0,0 +1,26 @@
+var toLoad = [
+{
+    id:"tall structures",
+    src:"https://raw.github.com/BlackScorp/tiled2crafty/gh-pages/kinetic/img/grassland_structures.png"
+},
+
+{
+    id:"water",
+    src:"https://raw.github.com/BlackScorp/tiled2crafty/gh-pages/kinetic/img/grassland_water.png"
+},
+
+{
+    id:"trees",
+    src:"https://raw.github.com/BlackScorp/tiled2crafty/gh-pages/kinetic/img/grassland_trees.png"
+},
+
+{
+    id:"grassland",
+    src:"https://raw.github.com/BlackScorp/tiled2crafty/gh-pages/kinetic/img/grassland.png"
+},
+
+{
+    id:"male player",
+    src:"https://raw.github.com/BlackScorp/tiled2crafty/gh-pages/kinetic/img/male_player.png"
+}
+];
--- kinetic/js/game/game.tmp.js
+++ kinetic/js/game/game.tmp.js
@@ -1,36 +1,78 @@
 var stage = null;
 $(function(){
-  
-    var toLoad = [
-    {
-        id:"tall structures",
-        src:"img/grassland_structures.png"
-    },
-
-    {
-        id:"water",
-        src:"img/grassland_water.png"
-    },
-
-    {
-        id:"trees",
-        src:"img/grassland_trees.png"
-    },
-
-    {
-        id:"grassland",
-        src:"img/grassland.png"
-    },
-
-    {
-        id:"male player",
-        src:"img/male_player.png"
+    var gameDiv = $('#game');
+    var h = gameDiv.height();
+    var w = gameDiv.width();
+    stage = new Kinetic.Stage({
+        container:'game',
+        width:w,
+        height:h
+    });
+    
+    var loadingScreen = new Kinetic.Layer();
+    var loadingBG = new Kinetic.Rect({
+        width:w,
+        height:h,
+        fill:"#333333"
+    });
+    var loadingBarBG = new Kinetic.Rect({
+        width:406,
+        height:24,
+        x:w/2-250,
+        y:h/2-12,
+        fill:'#232323',
+        cornerRadius:2
+    });
+    loadingScreen.add(loadingBG);
+    loadingScreen.add(loadingBarBG);
+   
+    
+    var loadingPoints = new Kinetic.Group();
+ 
+    for(var i = 0;i<100;i++){
+        var loadingPoint = new Kinetic.Rect({
+            width:2,
+            height:16,
+            x:i*(4),
+            y:2,
+            name:i,
+            offset:[-(w/2-246),-(h/2-10)],
+            fill:'#818181'
+        });
+        loadingPoints.add(loadingPoint);
     }
-    ];
-    var loader = new Kinetic.Loader(toLoad);
+    var loadingText = new Kinetic.Text({
+        x: 0,
+        y: 0,
+        text: "0%",
+        fontSize: 10,
+        fontFamily: "Calibri",
+        textFill: "#FFFFFF",
+        offset:[-(w/2-246),-(h/2+28)]
+    });
+        
+    loadingPoints.add(loadingText);
+    loadingScreen.add(loadingPoints);
+    
 
+    stage.add(loadingScreen);
+    var loader = new Kinetic.Loader(toLoad);
+    
     loader.onProgress(function(data){
-        console.log(data);
+        for(var i = 0,il = data.percent;i<il;i++){
+            var point = loadingPoints.children[i];
+            if(point){
+                point.attrs.height = 16;
+                point.attrs.fill = '#FFFFFF'; 
+            }
+          
+        }
+        point.attrs.height =30;
+        var text = loadingPoints.children[100];
+        text.attrs.x = (data.percent*4)-(16);
+        text.setText(data.percent+'%');
+        
+        loadingScreen.draw(); 
     });
     loader.onError(function(data){
         console.warn("Error on Load "+data.name);
@@ -42,6 +84,7 @@ $(function(){
     });
  
     loader.load();
+     
 });
 
 var Game = function(){
@@ -67,12 +110,12 @@ Game.prototype.load = function(file){
 Game.prototype.render = function(diff){
   
     
-}
+    }
 Game.prototype.update = function(){
     
    
     
-}
+    }
 Game.prototype.run = function(data){
  
     //create Map
